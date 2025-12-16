787b366d4857e7b2d7506b5f312db5417102acf8
fenix99355 <fenix99355@gmail.com>
2025-12-16 19:48:09 +0100

    Cambio stack, refactor, corregida tabla stats y selectores fechas

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://anvrbqvdswjexseirxhb.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFudnJicXZkc3dqZXhzZWlyeGhiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU1NDIwMDYsImV4cCI6MjA4MTExODAwNn0.6OStsGMiRnhP4f0xj_Gj54KaUh2HbDEBmLAO2kGXG04'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
